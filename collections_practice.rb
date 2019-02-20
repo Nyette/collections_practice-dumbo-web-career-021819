e28bbf91d0bed3fdbfddf7281bc5b462a1f80573
Antoinette Lee <antoinette.lee02@myhunter.cuny.edu>
2019-02-20 21:32:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,6 +1,21 @@
 def sort_array_asc(array)
   array.sort
 end
+<<<<<<< HEAD
+=======
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+>>>>>>> 231ef849f3b48052b55601fa69d5ac1fa2acd6e1
 
 def sort_array_desc(array)
   array.sort do |a, b|
